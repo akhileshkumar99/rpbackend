3300c13903720d3847b6b17d32ff534273adeb4b
akhileshkumar99 <ayodhyaexpress123@gmail.com>
2026-02-17 14:15:40 +0530

    fix cors

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const { Admin, Gallery, HeroSlide, Faculty, Course, Admission, Contact, Notice, 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // uploads folder static
